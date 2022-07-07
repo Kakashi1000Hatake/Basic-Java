616188227b6a890ebb781f9f81e8979198225859
Harsh Lakhanpal <92823514+Kakashi1000Hatake@users.noreply.github.com>
2022-07-07 11:33:53 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,17 @@
+public class factorial {
+    public static void main(String[] args) {
+
+        // finding the factorial of a number 
+
+        // int i, fact=1;
+        // int num = 5;// thi is the number  to be factorial of 
+
+        // for(i=1; i<=num; i++){
+        //     fact = fact*i;
+
+        // }
+
+        // System.out.println("Factorial of number "+num+" is : "+fact);
+
+    }
+}
