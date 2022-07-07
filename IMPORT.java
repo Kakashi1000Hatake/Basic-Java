a80c04e096e8e8b270f665b956b39da8c725b0be
Harsh Lakhanpal <92823514+Kakashi1000Hatake@users.noreply.github.com>
2022-07-07 11:34:10 +0530

    Add files via upload

---
+++ IMPORT.java
@@ -0,0 +1,67 @@
+// import java.lang.*;
+
+public class IMPORT {
+    public static void main(String[] args) {
+
+
+        // Maths operations 
+
+        // int a = 40;
+        // int b = 30;
+        // int x = 70;
+        // int y = 20;
+        
+        // System.out.println(Math.max(a, b)); //maximum
+        // System.out.println(Math.min(a, b));//minimum
+        // System.out.println(Math.sqrt(a));//Square root
+        // System.out.println(Math.abs(-10));//absolute
+        // System.out.println(Math.cbrt(a));//cube root
+        // System.out.println(Math.ceil(19.37));// round off
+        // System.out.println(Math.pow(a, b));// to give the power 
+        // System.out.println(Math.random());// random select
+        // System.out.println(Math.addExact(x, y));// add
+        // System.out.println(Math.subtractExact(x, y));// subtract
+        // System.out.println(Math.multiplyExact(x, y));// multiply
+        // System.out.println(Math.floorDiv(x, y));// divide
+        // System.out.println(Math.sin(a));
+        // System.out.println(Math.cos(a));
+        // System.out.println(Math.tan(a));
+        // System.out.println(Math.asin(a)); // to reverse the sin i.e cose
+        // System.out.println(Math.acos(a)); // to reverse the cos i.e sec
+        // System.out.println(Math.atan(a)); // to reverse the tan i.e cot
+
+
+
+        // String functions 
+
+        // String s = "Harsh Lakhanpal";
+        // System.out.println(s.toUpperCase());// to change all the character of string to Upper Case
+        // System.out.println(s.toLowerCase());// to change all the character of string to Lower Case
+        // System.out.println();
+        // System.out.println(s);
+
+
+        //String buffer
+        // StringBuffer sb = new StringBuffer(s); // calling the String Buffer class and creating the object
+        // sb.reverse();// this statement reverse the string
+        // System.out.println(sb); // to print the String in reverse
+
+        // System.out.println(s.length());// to print the length of the string
+        // String a = s.replace("Harsh", "Sham");// to replace the element(Character) of string to some other element(Character)
+        // System.out.println(a);// printing the replaced character string
+
+        // String txt = " Please locate where locate Harsh occures ";// creating a new string to chect the occurence of a certain character in the string
+        // System.out.println(txt.indexOf("locate"));// prints the position of first occurence of the element
+
+        // System.out.println(txt.lastIndexOf("Harsh"));//prints the position of last occurence of the element
+
+        // System.out.println(s.charAt(6));// to print the character on the given position
+
+        // System.out.println(s.substring(1,4)); // to extract striing inside other string
+
+        // System.out.println(txt.trim());// trim is used to remove the spaces between the words from the start and end
+        // System.out.println(txt);// this is the statement with spacesc in start and end
+
+
+    }
+}
