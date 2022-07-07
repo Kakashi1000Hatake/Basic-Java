662850bcbdadaa28c64933c332eac20489882d74
Harsh Lakhanpal <92823514+Kakashi1000Hatake@users.noreply.github.com>
2022-07-07 11:35:19 +0530

    Add files via upload

---
+++ tyypecasting.java
@@ -0,0 +1,32 @@
+public class tyypecasting {
+    public static void main(String[] args) {
+
+        // Widening (Typecasting)
+
+        // int a = 10;
+        // double b = a; // this creates the integer to double
+        // System.out.println(a);// this is int
+        // System.out.println(b);// this prints double
+
+
+        // narrowing (Typecasting)
+
+        // double a = 10.33;
+        // int b = (int)a; // this creates the double to int
+
+        // System.out.println(a); // this is double
+        // System.out.println(b); // this is int
+
+
+        // convetrting integer to String 
+        
+        // int a = 10;
+        // String b = String.valueOf(a);
+
+        // System.out.println(a+100); // addition of integers
+        // System.out.println(b+100); // addition of strings
+
+
+
+    }
+}
